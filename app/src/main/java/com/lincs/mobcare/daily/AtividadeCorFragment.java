452658c0156a306ecf4053a851e5e16285fb9bf4
JuliaFC <mjfc@cin.ufpe.br>
2018-12-08 07:25:44 -0300

    Terminando o módulo BluetoothConnectionService

--- app/src/main/java/com/lincs/mobcare/daily/AtividadeCorFragment.java
+++ app/src/main/java/com/lincs/mobcare/daily/AtividadeCorFragment.java
@@ -3,22 +3,15 @@ package com.lincs.mobcare.daily;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Message;
-import android.os.ParcelUuid;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,24 +21,29 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.lincs.mobcare.MainActivity;
 import com.lincs.mobcare.R;
+import com.lincs.mobcare.utils.BluetoothConnectionService;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 public class AtividadeCorFragment extends Fragment {
 
-    private final static int REQUEST_ENABLE_BT = 1;
-    private final static int MY_PERMISSIONS_COARSE_LOCATION = 1;
-    private final static String TAG = "Atividade Cor === ";
-    private final static UUID MY_UUID_UNSECURE = UUID.fromString("a8dd6c1b-0908-42c8-b2ae-32133f068a0b ");
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int MY_PERMISSIONS_COARSE_LOCATION = 1;
+    private static final UUID MY_UUID_INSECURE =
+            UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
+    private static final String TAG = "Atividade Cor === ";
+
     private TextView message;
+    public ArrayList<BluetoothDevice> scannedDevices = new ArrayList<BluetoothDevice>();
+    private BluetoothDevice chosenDevice;
     private BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private BluetoothConnectionService mConnectionService;
     private ArrayAdapter<String> adapterList;
     private ArrayList<String> scannedDevicesNames = new ArrayList<String>();
     private TextView bluetoothStatus;
@@ -74,12 +72,18 @@ public class AtividadeCorFragment extends Fragment {
         return;
     }
 
+    private void SendMessage(){
+        byte[] bytes = message.toString().getBytes(Charset.defaultCharset());
+        mConnectionService.write(bytes);
+    }
+
     private void SetUpColorButtons(){
 
         vermelho.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 message.setText("VERMELHO");
+                SendMessage();
             }
         });
 
@@ -87,6 +91,7 @@ public class AtividadeCorFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 message.setText("AMARELO");
+                SendMessage();
             }
         });
 
@@ -94,6 +99,7 @@ public class AtividadeCorFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 message.setText("VERDE");
+                SendMessage();
             }
         });
 
@@ -101,6 +107,7 @@ public class AtividadeCorFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 message.setText("AZUL");
+                SendMessage();
             }
         });
 
@@ -128,13 +135,14 @@ public class AtividadeCorFragment extends Fragment {
         lv.setAdapter(adapterList);
     }
 
-    private final BroadcastReceiver bReciever = new BroadcastReceiver() {
+    private final BroadcastReceiver scanBroadcastReceiver= new BroadcastReceiver() {
 
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             Log.d(TAG, "ON HERE");
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                scannedDevices.add(device);
                 if(device.getName() != null) {
                     Log.d(TAG, device.getName());
                     ScanDevices(device.getName() + ": " + device.getAddress());
@@ -146,12 +154,44 @@ public class AtividadeCorFragment extends Fragment {
         }
     };
 
+    private final BroadcastReceiver pairBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if(action.equals(BluetoothDevice.ACTION_BOND_STATE_CHANGED)){
+                BluetoothDevice mDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                //3 cases:
+                //case1: bonded already
+                if (mDevice.getBondState() == BluetoothDevice.BOND_BONDED){
+                    Log.d(TAG, "BroadcastReceiver: BOND_BONDED.");
+                    //inside BroadcastReceiver4
+                    chosenDevice= mDevice;
+                }
+                //case2: creating a bone
+                if (mDevice.getBondState() == BluetoothDevice.BOND_BONDING) {
+                    Log.d(TAG, "BroadcastReceiver: BOND_BONDING.");
+                }
+                //case3: breaking a bond
+                if (mDevice.getBondState() == BluetoothDevice.BOND_NONE) {
+                    Log.d(TAG, "BroadcastReceiver: BOND_NONE.");
+                }
+            }
+        }
+    };
+
+    public void startBTConnection(BluetoothDevice device, UUID uuid){
+        Log.d(TAG, "startBTConnection: Initializing RFCOM Bluetooth Connection.");
+
+        mConnectionService.startClient(device,uuid);
+    }
     @Override
     public void onDestroy() {
         super.onDestroy();
 
         // Don't forget to unregister the ACTION_FOUND receiver.
-        getActivity().unregisterReceiver(bReciever);
+        getActivity().unregisterReceiver(scanBroadcastReceiver);
+        getActivity().unregisterReceiver(pairBroadcastReceiver);
     }
 
     @Nullable
@@ -170,10 +210,23 @@ public class AtividadeCorFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
                                     long id) {
-                Object obj = parent.getItemAtPosition(position);
-                String name = obj.toString();
+
+                String name = scannedDevices.get(position).getName();
                 message.setText(name);
                 mBluetoothAdapter.cancelDiscovery();
+
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
+                getActivity().registerReceiver(pairBroadcastReceiver, filter);
+
+                scannedDevices.get(position).createBond();
+
+                chosenDevice = scannedDevices.get(position);
+                mConnectionService = new BluetoothConnectionService(getActivity());
+
+                startBTConnection(chosenDevice, MY_UUID_INSECURE);
+
+
+
 
 
             }
@@ -189,7 +242,7 @@ public class AtividadeCorFragment extends Fragment {
                 mBluetoothAdapter.startDiscovery();
 
                 IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                getActivity().registerReceiver(bReciever, filter);
+                getActivity().registerReceiver(scanBroadcastReceiver, filter);
 
                 bluetoothStatus.setText("Started Discovery");
                 Log.d(TAG, "PRESSED BUTTON SCAN\n");
@@ -217,6 +270,7 @@ public class AtividadeCorFragment extends Fragment {
 
         return view;
     }
+
 
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
